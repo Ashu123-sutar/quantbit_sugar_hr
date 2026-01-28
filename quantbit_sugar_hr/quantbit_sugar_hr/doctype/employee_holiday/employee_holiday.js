1f0d8b4edd0b155e3453a897a55d830cb46bec7d
Your Name <you@example.com>
2026-01-28 09:40:23 +0530

    Changes in Code

--- quantbit_sugar_hr/quantbit_sugar_hr/doctype/employee_holiday/employee_holiday.js
+++ quantbit_sugar_hr/quantbit_sugar_hr/doctype/employee_holiday/employee_holiday.js
@@ -1,8 +1,79 @@
-// Copyright (c) 2026, Quantbit Technologies and contributors
+// Copyright (c) 2025, Quantbit Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Employee Holiday", {
-// 	refresh(frm) {
+function getDayNameFromDate(input_date) {
 
-// 	},
-// });
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const d = new Date(input_date);
+    const dayName = days[d.getDay()];
+
+    return dayName;
+}
+frappe.ui.form.on("Employee Holiday", {
+    from_date: function (frm) {
+        if (frm.doc.from_date && frm.doc.to_date) {
+            frm.call({
+                doc: frm.doc,
+                method: "add_dates",
+                callback: function (r) {
+                    frm.refresh_field("holiday_date_details")
+                }
+            })
+        }
+    },
+    to_date: function (frm) {
+        if (frm.doc.from_date && frm.doc.to_date) {
+            frm.call({
+                doc: frm.doc,
+                method: "add_dates",
+                callback: function (r) {
+                    frm.refresh_field("holiday_date_details")
+                }
+            })
+        }
+    },
+    season_type: function (frm) {
+        if (frm.doc.from_date && frm.doc.to_date) {
+            frm.call({
+                doc: frm.doc,
+                method: "add_dates",
+                callback: function (r) {
+                    frm.refresh_field("holiday_date_details")
+                }
+            })
+        }
+    },
+    branch: function (frm) {
+        frm.call({
+            doc: frm.doc,
+            method: "add_staff",
+            callback: function (r) {
+                frm.refresh_field("employee_holiday_details")
+            }
+        })
+    },
+    company: function (frm) {
+        frm.call({
+            doc: frm.doc,
+            method: "add_staff",
+            callback: function (r) {
+                frm.refresh_field("employee_holiday_details")
+            }
+        })
+    },
+    select_all: function (frm) {
+        frm.dirty()
+        let child_table_field_name = "holiday_date_details";
+
+        if (frm.doc[child_table_field_name] && frm.doc[child_table_field_name].length > 0) {
+            let any_unchecked = frm.doc[child_table_field_name].some(row => !row.check);
+            let new_check_state = any_unchecked ? 1 : 0;
+
+            frm.doc[child_table_field_name].forEach(function (row) {
+                row.check = new_check_state;
+            });
+            frm.refresh_field(child_table_field_name);
+        }
+    },
+});
